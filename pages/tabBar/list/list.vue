fc7ba09d82bd75a3dde72edf862084cae96c6124
18082227 <2wsx!QAZ>
2019-12-12 18:49:47 +0800

    正式开始

---
+++ pages/tabBar/list/list.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<view class="zzjz-list-top zzjz-bg-red">
+			<view class="zzjz-list-top-a">年月</view>
+			<view class="zzjz-list-top-b">收入|支出</view>
+		</view>
+		<view class="zzjz-list-list zzjz-bg-blue">
+			<view class="zzjz-list-list-c">1</view>
+			<view class="zzjz-list-list-c">2</view>
+			<view class="zzjz-list-list-c">3</view>
+			<view class="zzjz-list-list-c">4</view>
+			<view class="zzjz-list-list-c">5</view>
+			<view class="zzjz-list-list-c">6</view>
+			<view class="zzjz-list-list-c">7</view>
+			<view class="zzjz-list-list-c">8</view>
+			<view class="zzjz-list-list-c">9</view>
+			<view class="zzjz-list-list-c">10</view>
+			<view class="zzjz-list-list-c">11</view>
+			<view class="zzjz-list-list-c">12</view>
+			<view class="zzjz-list-list-c">13</view>
+			<view class="zzjz-list-list-c">14</view>
+			<view class="zzjz-list-list-c">15</view>
+			<view class="zzjz-list-list-c">16</view>
+			<view class="zzjz-list-list-c">17</view>
+			<view class="zzjz-list-list-c">18</view>
+			<view class="zzjz-list-list-c">19</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+	.zzjz-list-top {
+		height: 150upx;
+		width: 100%;
+		position: fixed;
+	}
+	.zzjz-list-top-a {
+		height: 150upx;
+		width: 30%;
+		display: inline-block;
+	}
+	.zzjz-list-top-b {
+		height: 150upx;
+		width: 70%;
+		display: inline-block;
+	}
+	.zzjz-list-list {
+		width: 100%;
+		position: absolute;
+		top: 150upx;
+		z-index: -1;
+	}
+	.zzjz-list-list-c {
+		height: 150upx;
+		width: 100%;
+	}
+</style>
