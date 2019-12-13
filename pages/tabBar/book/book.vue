9e73d7508de307e07a8a0202b6960f504747b27c
18082227 <2wsx!QAZ>
2019-12-13 16:06:08 +0800

    11

--- pages/tabBar/book/book.vue
+++ pages/tabBar/book/book.vue
@@ -1,8 +1,68 @@
 <template>
+	<view>
+		<view class="zzjz-book-top zzjz-bg-red">
+			<view class="zzjz-book-top-a">结余</view>
+			<view class="zzjz-book-top-b">收入|支出</view>
+		</view>
+		<view class="zzjz-book-title zzjz-bg-blue">
+		</view>
+		<view class="zzjz-book-list zzjz-bg-green">
+			<view class="zzjz-book-list-c">1</view>
+			<view class="zzjz-book-list-c">2</view>
+			<view class="zzjz-book-list-c">3</view>
+			<view class="zzjz-book-list-c">4</view>
+			<view class="zzjz-book-list-c">5</view>
+			<view class="zzjz-book-list-c">6</view>
+			<view class="zzjz-book-list-c">7</view>
+			<view class="zzjz-book-list-c">8</view>
+			<view class="zzjz-book-list-c">9</view>
+			<view class="zzjz-book-list-c">10</view>
+			<view class="zzjz-book-list-c">11</view>
+			<view class="zzjz-book-list-c">12</view>
+			<view class="zzjz-book-list-c">13</view>
+			<view class="zzjz-book-list-c">14</view>
+			<view class="zzjz-book-list-c">15</view>
+			<view class="zzjz-book-list-c">16</view>
+			<view class="zzjz-book-list-c">17</view>
+			<view class="zzjz-book-list-c">18</view>
+			<view class="zzjz-book-list-c">19</view>
+		</view>
+	</view>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss">
+	.zzjz-book-top {
+		height: 300upx;
+		width: 100%;
+		position: fixed;
+	}
+	.zzjz-book-top-a {
+		height: 200upx;
+		width: 100%;
+		display: absolute;
+	}
+	.zzjz-book-top-b {
+		height: 100upx;
+		width: 100%;
+		display: absolute;
+	}
+	.zzjz-book-title {
+		height: 50upx;
+		width: 100%;
+		position: fixed;
+		top: 300upx;
+	}
+	.zzjz-book-list {
+		width: 100%;
+		position: absolute;
+		top: 350upx;
+		z-index: -1;
+	}
+	.zzjz-book-list-c {
+		height: 150upx;
+		width: 100%;
+	}
 </style>
