d90ab2fd8a9fb7ad2fff861d3a04d57d7b15319f
18082227 <2wsx!QAZ>
2019-12-17 19:00:56 +0800

    11

--- App.vue
+++ App.vue
@@ -14,5 +14,5 @@
 
 <style>
 	/*每个页面公共css */
-	@import "./common/hello.css";
+	@import "./common/zzjz.css";
 </style>
--- pages/tabBar/add/add.vue
+++ pages/tabBar/add/add.vue
@@ -1,18 +1,64 @@
 <template>
 	<view>
-		<view class="zzjz-add-top zzjz-bg-red">
-			<view class="zzjz-add-top-expenses zzjz-flex-grow-1 zzjz-bg-blue">收入</view>
-			<view class="zzjz-add-top-income zzjz-flex-grow-1 zzjz-bg-green">支出</view>
+		<view class="top zzjz-bg-blue">
+			<view class="top-expenses zzjz-flex-grow-1 ">收入</view>
+			<view class="top-income zzjz-flex-grow-1 ">支出</view>
 		</view>
-		<view class="zzjz-add-form"></view>
+		<view class="form">
+			<view class="form-type zzjz-bg-red">
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<image @tap="formTypeimgClick" @longtap="formTypeimgLongTap" class="form-typeimg zzjz-bg-green"></image>
+				<i></i><i></i><i></i><i></i><i></i>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	var longClick = false;
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			formTypeimgClick: function (e) {
+				if (longClick) {
+					console.log('longtap');
+				} else{
+					console.log('tap');
+				}
+				longClick = false;
+			},
+			formTypeimgLongTap: function (e) {
+				longClick = true;
+			}
+		}
+	}
+	// Page({
+	//   formTypeimgClick: function (e) {
+	//     console.log('11111111');
+	//   }
+	// })
 </script>
 
 <style lang="scss">
-	.zzjz-add-top {
+	.top {
 		height: 150upx;
 		width: 100vw;
 		position: fixed;
@@ -21,9 +67,33 @@
 		justify-content: space-between;
 		align-items: center;
 	}
-	.zzjz-add-top-expenses .zzjz-add-top-income {
+	.top-expenses .top-income {
 		height: 150upx;
 		font-size: 30upx;
+	}
+	.form {
+	}
+	.form-type {
+		top: 150upx;
+		width: 100vw;
+		position: absolute;
+		display: flex;
+		flex-flow: wrap;
+		justify-content: space-between;
+		z-index: -1;
+	}
+	// .form-type:after {
+	// 	content: '';
+	// 	flex: auto;
+	// }
+	.form-type > i {
+		width: 120upx;
+		margin-right: 5upx;
+	}
+	.form-typeimg {
+		height: 120upx;
+		width: 120upx;
+		margin: 10upx 5upx 0 5upx;
 	}
 	
 </style>
